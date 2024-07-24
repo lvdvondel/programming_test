fe6457087bf2d4b3205cd4c296a21e4fdc16786b
lvdvondel <lievenvandevondel@hotmail.com>
2024-07-24 22:19:26 +0200

    add own coordinates to initiation of player and npc

--- Player_and_NPC.py
+++ Player_and_NPC.py
@@ -1,12 +1,15 @@
 # imports
 
 class Player(object):
-    
-    def __init__(self):
-        
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z    
 
 class Generic_npc(object):
-    
-    def __init__(self):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
 #main
