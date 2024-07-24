0c9ba408c46f095b7c6435707e0f7414f9287f06
lvdvondel <lievenvandevondel@hotmail.com>
2024-07-24 22:28:43 +0200

    add access to coordinate properties

--- Player_and_NPC.py
+++ Player_and_NPC.py
@@ -4,7 +4,12 @@ class Player(object):
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
-        self.z = z    
+        self.z = z
+
+    def get_coordinates(self):
+        print("x = " +  str(self.x) + "y = "+ str(self.y) + "z = " + str(self.z))
+        coord = {"x":self.x, "y":self.y, "z":self.z}
+        return(coord)
 
 class Generic_npc(object):
     def __init__(self, x, y, z):
@@ -12,4 +17,9 @@ class Generic_npc(object):
         self.y = y
         self.z = z
 
+    def get_coordinates(self):
+        print("x = " +  str(self.x) + "y = "+ str(self.y) + "z = " + str(self.z))
+        coord = {"x":self.x, "y":self.y, "z":self.z}
+        return(coord)
+    
 #main
