31f7b1dbc6a48d24d13504985aa5ca599a422fa3
lvdvondel <lievenvandevondel@hotmail.com>
2024-07-25 10:48:03 +0200

    debug player and NPC code, add functionality

--- Player_and_NPC.py
+++ Player_and_NPC.py
@@ -1,25 +1,45 @@
 # imports
 
-class Player(object):
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def get_coordinates(self):
-        print("x = " +  str(self.x) + "y = "+ str(self.y) + "z = " + str(self.z))
-        coord = {"x":self.x, "y":self.y, "z":self.z}
-        return(coord)
-
-class Generic_npc(object):
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def get_coordinates(self):
-        print("x = " +  str(self.x) + "y = "+ str(self.y) + "z = " + str(self.z))
-        coord = {"x":self.x, "y":self.y, "z":self.z}
-        return(coord)
+class Living_Object():
+    """This class is a parent for npcs and playable characters"""
     
+    #constructor
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    #print object info
+    def __str__(self):
+        rep = "This is an object that can move. The current location [x, y, z] is:"
+        rep = rep + "[" + str(self.x) + ", " + str(self.y) + ", " + str(self.z) + "]"
+        return rep
+    
+    def get_coordinates(self):
+        Coord_lib = {"x": self.x, "y": self.y, "z": self.z}
+        return(Coord_lib)
+
+class Player_character(Living_Object):
+    """This class inherits from Living_Object and is controlled by the player"""
+    
+    #constructor
+    def __init__(self, x, y, z, name):
+        super().__init__(x, y, z)
+        self.name = name
+
+class NPC(Living_Object):
+    """This class inherits from Living_Object and is computer controlled"""
+    
+    #count number of active NPC's to avoid overpopulation
+    count_NPC = 0
+    
+    #Constructor: 
+    def __init__(self, x, y, z, name):
+        super().__init__(x, y, z)
+        self.name = name
+        count_NPC +=1 #increase count of NPC's by 1 when a new NPC is constructed
+
 #main
+
+Bernard = Player_character(0,0,0, 'Bernard')
+print(Bernard.get_coordinates())
